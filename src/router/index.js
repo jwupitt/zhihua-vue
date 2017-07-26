652fc898d3f7702f4e752eb76a0c7697d893683a
Jia <jiawu.fe@hotmail.com>
2017-07-26 09:20:16 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: resolve => require.ensure([], () => resolve(require('../components/Hello.vue')), 'hello')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: resolve => require.ensure([], () => resolve(require('../components/test.vue')), 'test')
+    },
+  ],
+});
